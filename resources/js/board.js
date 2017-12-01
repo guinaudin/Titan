1074113b37c1e506ef1e51631245a22783068047
NAUDIN <guillaume.naudin@capgemini.com>
2017-12-01 18:41:07 +0100

    display js Board

--- resources/js/board.js
+++ resources/js/board.js
@@ -1,58 +1,82 @@
-function draw()
-{
-    canvas = document.getElementById('titan');
- 
-    // Canvas supported?
-    if(canvas.getContext)
-    {
-        ctx = canvas.getContext('2d');
- 
-        // Calculdate the precise block size
-        BLOCK_SIZE = canvas.height / NUMBER_OF_ROWS;
-         
-        // Draw the background
-        drawBoard();
- 
-        defaultPositions();
-         
-        // Draw players
-        
- 
-    }
-    else
-    {
-        alert("Canvas not supported!");
+var NB_ROW = 10;
+var NB_COL = 10;
+var board = new Array();
+
+var Pos = {
+    init: function(posX, posY) {
+        this.posX = posX;
+        this.posY = posY;
     }
 }
 
-function drawBoard()
-{   
-    for(iRowCounter = 0; iRowCounter < NUMBER_OF_ROWS; iRowCounter++)
-    {
-        drawRow(iRowCounter);
-    }   
-     
-    // Draw outline
-    ctx.lineWidth = 3;
-    ctx.strokeRect(0, 0, NUMBER_OF_ROWS * BLOCK_SIZE, NUMBER_OF_COLS * BLOCK_SIZE); 
-}
-
-function drawRow(iRowCounter)
-{
-    // Draw 8 block left to right
-    for(iBlockCounter = 0; iBlockCounter < NUMBER_OF_ROWS; iBlockCounter++)
-    {
-        drawBlock(iRowCounter, iBlockCounter);
+var Block = {
+    init: function(obj, value, state) {
+        this.obj = obj;
+        this.value = value;
+        this.state = state;
     }
 }
 
-function drawBlock(iRowCounter, iBlockCounter)
-{   
-    // Set the background
-    ctx.fillStyle = getBlockColour(iRowCounter, iBlockCounter);
-     
-    // Draw rectangle for the background
-    ctx.fillRect(iRowCounter * BLOCK_SIZE, iBlockCounter * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
- 
-    ctx.stroke();   
+var Player = {
+    init: function(posX, posY, power, toughness, life) {
+        this.posX = posX;
+        this.posY = posY;
+        this.power = power;
+        this.toughness = toughness;
+        this.life = life;
+    }
+}
+
+function display() {
+    initBoard();
+
+    player1 = Object.create(Player);
+    player1.init(findEmptyRandomPlace().posX, findEmptyRandomPlace().posY, 25, 100, 10);
+    board[player1.posX][player1.posY] = player1;
+    board[player1.posX][player1.posY].value = 1;
+    board[player1.posX][player1.posY].state = 1;
+
+    displayStringBoard();
+}
+
+function findEmptyRandomPlace() {
+    var freeBoardPlace = new Array();
+    var selectedBlock = Object.create(Pos);
+
+    for(i = 0; i < NB_ROW; i++) {
+        for(j = 0; j < NB_COL; j++) {
+            if(!board[i][j].state) {
+                var pos = Object.create(Pos);
+                freeBoardPlace.push(pos.init(i, j));
+            }
+        }
+    }
+
+    return freeBoardPlace[Math.random() * freeBoardPlace.length];
+}
+
+function initBoard() {
+     for(i = 0; i < NB_ROW; i++) {
+        board[i] = new Array();
+    }
+
+    for(i = 0; i < NB_ROW; i++) {
+        for(j = 0; j < NB_COL; j++) {
+            board[i][j] = Object.create(Block);
+            board[i][j].init(null, 0, 0);
+        }
+    }
+}
+
+function displayStringBoard() {
+    var boardString = "";
+
+    for(i = 0; i < NB_ROW; i++) {
+        for(j = 0; j < NB_COL; j++) {
+            boardString = boardString + "\t" + board[i][j].value;
+        }
+        boardString = boardString + "\n";
+    }
+
+    console.log(boardString);
 }
---
+++ resources/js/combat.js
@@ -0,0 +1,25 @@
+var Personnage = {
+    // Initialise le personnage
+    init: function (nom, sante, force) {
+        this.nom = nom;
+        this.sante = sante;
+        this.force = force;
+        this.xp = 0;
+    },
+
+    // Renvoie la description du personnage
+    decrire: function () {
+        var description = this.nom + " a " + this.sante + " points de vie, " +
+            this.force + " en force et " + this.xp + " points d'expérience";
+        return description;
+    }
+};
+
+var perso1 = Object.create(Personnage);
+perso1.init("Aurora", 150, 25);
+
+var perso2 = Object.create(Personnage);
+perso2.init("Glacius", 130, 30);
+
+console.log(perso1.decrire());
+console.log(perso2.decrire());
